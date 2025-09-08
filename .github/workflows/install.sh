99aee3612baf87eb88a5f2c948c4463ff6f06d09
Andrew Hwang <ahwang@cribl.io>
2025-09-08 09:30:17 -0700

    four

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -3,7 +3,7 @@ USE_HTTPS_GIT=$PUBLIC_REPO
 if [[ "$USE_HTTPS_GIT" == "true" ]]; then
 PACK_REPO_URL="$PACK_REPO_HTTPS_URL"
 else
-PACK_REPO_URL="git+https://$REPO_OWNER:$PACK_REPO_SSH_TOKEN@github.com/$REPO_NAME.git"
+PACK_REPO_URL="git+https://$REPO_OWNER:$PACK_REPO_SSH_TOK3N@github.com/$REPO_NAME.git"
 fi
 # Reads the list of worker groups to install the pack to
 WG_LIST="$(pwd)/$CRIBL_WG_LIST"
